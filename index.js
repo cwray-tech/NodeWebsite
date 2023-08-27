31e8435aacfbff9820232dff799ddfa349bcf960
Christopher Wray <chris@sol.company>
2023-08-26 19:04:40 -0700

    add html content and server pages

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ const app = express();
 const { readFile } = require("fs").promises;
 
 app.get("/", async (req, res) => {
-  res.send(await readFile("/pages/index.html", "utf8"));
+  res.send(await readFile("pages/index.html", "utf8"));
 });
+
+app.get("/about", async (req, res) => {
+  res.send(await readFile("pages/about.html", "utf8"));
+});
+
+app.get("/contact-me", async (req, res) => {
+  res.send(await readFile("pages/contact-me.html", "utf8"));
+});
+
+app.listen(3000, () => console.log("App available on http://localhost:3000"));
