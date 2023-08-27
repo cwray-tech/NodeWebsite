e66a1aada3ffe6007a99c8a77646ba2a391c5a8a
Christopher Wray <chris@sol.company>
2023-08-26 19:09:02 -0700

    add 404 page

--- index.js
+++ index.js
@@ -15,4 +15,8 @@ app.get("/contact-me", async (req, res) => {
   res.send(await readFile("pages/contact-me.html", "utf8"));
 });
 
+app.use(async (req, res) => {
+  res.status(404).send(await readFile("pages/404.html", "utf8"));
+});
+
 app.listen(3000, () => console.log("App available on http://localhost:3000"));
